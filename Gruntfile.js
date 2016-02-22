0ba325bd80f162ed9fbb4afed9888f2dcb7270cc
huning <304873772@qq.com>
2016-02-22 20:07:59 +0800

    添加头尾模块，构建grunt脚手架，添加基本css

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,44 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
+        less: {
+            development: {
+                files: [{
+                    expand: true,
+                    cwd: './public/stylesheets',
+                    src: ['**/*.less','!**/_*.less'],
+                    dest: './public/css',
+                    ext: '.css'
+                }]
+            }
+        },
+        // CSS压缩
+        cssmin: {
+            build: {
+                expand: true,
+                cwd: './public/css',
+                src: ['*.css', '!*.min.css'],
+                dest: './public/css',
+                ext: '.css'
+            }
+        }
+
+        /*uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/!*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> *!/\n'
             },
             build: {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
-        }
+        }*/
     });
 
     // 加载包含 "uglify" 任务的插件。
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // 默认被执行的任务列表。
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('stylesheets', ['less','cssmin']);
 
 };
