4f2d49e6e88693977686d87c113cb94092403068
huning <304873772@qq.com>
2016-02-24 18:28:29 +0800

    添加express-autoroute路由插件

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+module.exports.autoroute = {
+  'get' : {
+    '/' : showIndexView
+  }
+};
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+});*/
+function showIndexView(req,res){
+  res.render('index', { title: 'Express' });
+}
 
-module.exports = router;
+//module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+module.exports.autoroute = {
+  'get' : {
+    '/users' : showUserView
+  }
+};
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+});*/
+function showUserView(req,res){
+  res.send('respond with a resource');
+}
 
-module.exports = router;
+//module.exports = router;
